f53cb599b76cb3b7eed486918663ddd26128a039
FelipeDasr <felipe.heiker07@gmail.com>
2024-08-31 19:47:21 -0400

    feat: methods generated by sqlc

---
+++ internal/db/db_connection.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// Connection is the connection with the database
+var Connection *sql.DB;
+
+// StartDBConnection starts a connection to the database
+func StartConnection() {
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		defer db.Close()
+		log.Fatalf("Error opening database connection.\n\n%v", err)
+	}
+
+	ctx := context.Background()
+	Connection = db
+
+	if err = Connection.PingContext(ctx); err != nil {
+		defer Connection.Close()
+		log.Fatalf("Error pinging database.\n\n%v", err)
+	}
+}
